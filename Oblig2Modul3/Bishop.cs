e57ee340002b33f2aced749751c8daab9124b323
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2020-09-17 09:42:24 +0200

    Initial commit

---
+++ Oblig2Modul3/Bishop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig2Modul3
+{
+    public class Bishop : Piece
+    { 
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+
+        public Bishop(string symbol = "TRN") : base(symbol)
+        {
+        }
+    }
+}
---
+++ Oblig2Modul3/Rook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Oblig2Modul3
+{
+    public class Rook: Piece
+    {
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+
+        public Rook(string symbol = "LPR") : base(symbol)
+        {
+        }
+    }
+}
